0d413bffec8591a0c8f4f3f3906401774593c565
Chandima Ranaweera <chan4lk@gmail.com>
2023-10-23 01:55:33 +0530

    graphql project added

---
+++ GrapQL.API/Queries/Query.cs
@@ -0,0 +1,20 @@
+using System.Linq;
+using System.Security.Claims;
+using GrapQL.API.Models;
+
+namespace GrapQL.API.Queries
+{
+    public class Query
+    {
+        public Book GetBook(ClaimsPrincipal claimsPrincipal) =>
+            new Book
+            {
+                Title = "C# in depth.",
+                Author = new Author
+                {
+                    Name = claimsPrincipal.Claims.FirstOrDefault(c=>  c.Type == ClaimTypes.Name)?.Value 
+                }
+            };
+    }
+
+}
